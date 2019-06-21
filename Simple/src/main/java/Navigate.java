6639f57e33263c77587ddeaf9e263be01d8fafd4
Nithya <Nithya@LAPTOP-LTLBT54P>
2019-06-21 10:32:04 +0530

    my first github code

---
+++ Simple/src/main/java/Navigate.java
@@ -0,0 +1,22 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigate {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+	String 	url="https://www.gmail.com";
+		driver.get(url);
+		
+		
+		
+		
+	
+	
+	}
+
+}
